73bb52c57156501cc0a23f43fa792e77a7bd82e4
Carlos Mendoza <Carlos.Mendoza@gmail.com>
2023-07-16 16:43:32 -0600

    Programa funcionando correctamente

--- src/org/cmendoza/pooherencia/almacenverdularia/EjemploMain.java
+++ src/org/cmendoza/pooherencia/almacenverdularia/EjemploMain.java
@@ -10,6 +10,28 @@ public class EjemploMain {
         productos[2] = new Limpieza("Detergente liquido",20,"Salvo",80);
         productos[3] = new NoPerecible(120,64,"Atun",25);
 
+        for (Producto p: productos) {
+            System.out.println("Nombre: " + p.getNombre());
+            System.out.println("Precio: " + p.getPrecio() + " $");
+
+            if (p instanceof Lacteo ){
+                System.out.println("Cantidad: " + ((Lacteo) p).getCantidad() + " G");
+                System.out.println("Proteinas: " + ((Lacteo) p).getProteinas() + " G");
+            } else if (p instanceof  Fruta) {
+                System.out.println("Color: " + ((Fruta) p).getColor());
+                System.out.println("Peso: " + p.getPrecio() + " kg");
+            } else if (p instanceof  Limpieza) {
+                System.out.println("Componentes: " + ((Limpieza) p).getComponentes());
+                System.out.println("Litros: " + ((Limpieza) p).getLitros());
+            } else if (p instanceof NoPerecible) {
+                System.out.println("Contenido: " + ((NoPerecible) p).getContenido() +  " G:" );
+                System.out.println("Calorías: " + ((NoPerecible) p).getCalorias());
+            }
+
+            System.out.println("================== Siguiente Producto ==================");
+
+        }
+
 
     }
 }
