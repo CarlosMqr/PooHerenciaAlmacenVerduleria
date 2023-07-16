f1d9f50be1166f692dd190977f0ad025bdcc254c
Carlos Mendoza <Carlos.Mendoza@gmail.com>
2023-07-16 14:06:37 -0600

    se crearon las clases y el metodo main

---
+++ src/org/cmendoza/pooherencia/almacenverdularia/EjemploMain.java
@@ -0,0 +1,7 @@
+package org.cmendoza.pooherencia.almacenverdularia;
+
+public class EjemploMain {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/org/cmendoza/pooherencia/almacenverdularia/modelo/Producto.java
@@ -0,0 +1,4 @@
+package org.cmendoza.pooherencia.almacenverdularia.modelo;
+
+public class Producto {
+}
